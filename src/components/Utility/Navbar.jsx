a59f2b95b58d34d337621494d1b233ccfa35f6af
ahmed <mommentcraft@gmail.com>
2023-08-03 12:10:20 +0300

    edit

--- src/components/Utility/Navbar.jsx
+++ src/components/Utility/Navbar.jsx
@@ -16,33 +16,50 @@ const NavBar = () => {
     setSlider((slider) => !slider);
     console.log(slider);
   }
+  function closeslide() {
+    setSlider(false);
+    console.log("yes");
+  }
   //nav click active
-  const [one, setOne] = useState(false);
-  const [two, setTwo] = useState(false);
-  const [tree, setTree] = useState(false);
-  const [four, setFour] = useState(false);
+  // const [one, setOne] = useState(false);
+  // const [two, setTwo] = useState(false);
+  // const [tree, setTree] = useState(false);
+  // const [four, setFour] = useState(false);
 
-  useEffect(() => {
-    setOne(true);
-  }, []);
-
+  // useEffect(() => {
+  //   setOne(true);
+  // }, []);
 
   return (
     <div className={navbar ? "bg-nav active-bg" : "bg-nav"}>
       <Container>
-        <div
-          className="con-nav"
-        >
+        <div className="con-nav">
           <div className="logo">
             <h3>ahmed</h3>
           </div>
           <nav className={slider ? " open-slider" : null}>
             <ul>
               <span>ahmed</span>
-              <CustomList to="/">Home</CustomList>
-              <CustomList to="/about">About</CustomList>
-              <CustomList to="/projects">Projects</CustomList>
-              <CustomList to="/contact">Contact</CustomList>
+              <CustomList to="/">
+                <Link to="/" onClick={closeslide}>
+                  Home
+                </Link>
+              </CustomList>
+              <CustomList to="/about">
+                <Link to="/about" onClick={closeslide}>
+                  About
+                </Link>
+              </CustomList>
+              <CustomList to="/projects">
+                <Link to="/projects" onClick={closeslide}>
+                  Projects
+                </Link>
+              </CustomList>
+              <CustomList to="/contact">
+                <Link to="/contact" onClick={closeslide}>
+                  Contact
+                </Link>
+              </CustomList>
             </ul>
           </nav>
           <div className="check-btn" onClick={openSlider}>
@@ -59,10 +76,6 @@ const CustomList = ({ to, children }) => {
   const resolvePath = useResolvedPath(to);
   const IsActive = useMatch(resolvePath.pathname);
 
-  return (
-    <li className={IsActive ? "active" : ""}>
-      <Link to={to}>{children}</Link>
-    </li>
-  );
+  return <li className={IsActive ? "active" : ""}>{children}</li>;
 };
 export default NavBar;
